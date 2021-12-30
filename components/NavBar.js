a312f1ad98f9b31a29a6117fe4d9d680deb58a3d
jiwlee <ddazi9576@google.com>
2021-12-30 16:45:42 +0900

    Component Styles with Styled JSX

--- components/NavBar.js
+++ components/NavBar.js
@@ -15,6 +15,20 @@ const NavBar = () => {
           </Link>
         </li>
       </ul>
+      <style jsx>{`
+        ul {
+          list-style-type: none;
+          padding: 0;
+        }
+
+        li {
+          display: inline;
+        }
+
+        li:not(:first-child) {
+          margin-left: 0.75rem;
+        }
+      `}</style>
     </nav>
   );
 };

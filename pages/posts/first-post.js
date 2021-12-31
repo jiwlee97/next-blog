dade0397ae3810b5625c4811c212ad5d0391d36a
jiwlee <ddazi9576@google.com>
2021-12-31 14:17:56 +0900

    First Post Page

---
+++ pages/posts/first-post.js
@@ -0,0 +1,19 @@
+import Head from "next/head";
+
+const FirstPostPage = () => {
+  console.log("[FirstPostPage] render");
+  return (
+    <>
+      <Head>
+        <title>First Post - My Blog</title>
+      </Head>
+
+      <main>
+        <h1>First Post</h1>
+        <p>This is my first ever blog post!</p>
+      </main>
+    </>
+  );
+};
+
+export default FirstPostPage;

7f8891d1c0977bf76818cdd94b74ac4dac828aac
jiwlee <ddazi9576@google.com>
2022-01-06 15:15:47 +0900

    ThemeSwitch Component

---
+++ components/ThemeSwitch.js
@@ -0,0 +1,28 @@
+import { useState, useCallback } from "react";
+
+const ThemeSwitch = () => {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const toggleTheme = useCallback(() => {
+    setDarkMode((prev) => !prev);
+  }, []);
+
+  console.log("[ThemeSwitch] darkMode: ", darkMode);
+
+  return (
+    <>
+      <button type="button" onClick={toggleTheme}>
+        {darkMode ? "Light Mode" : "Dark Mode"}
+      </button>
+      <style jsx>{`
+        button {
+          background: none;
+          border: none;
+          cursor: pointer;
+        }
+      `}</style>
+    </>
+  );
+};
+
+export default ThemeSwitch;
